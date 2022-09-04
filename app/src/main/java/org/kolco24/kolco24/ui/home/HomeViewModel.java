efdd66ad4a6bab3f4149992a488ab21d6a4e84aa
alff0x1f <alff0x1f@gmail.com>
2022-09-04 11:11:50 +0500

    Init

---
+++ app/src/main/java/org/kolco24/kolco24/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package org.kolco24.kolco24.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/org/kolco24/kolco24/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package org.kolco24.kolco24.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
