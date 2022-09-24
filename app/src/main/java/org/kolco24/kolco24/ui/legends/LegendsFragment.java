85ecf37218808b601c852eeb4f6df0b926a584d4
alff0x1f <alff0x1f@gmail.com>
2022-09-24 21:54:17 +0500

    Internet request

--- app/src/main/java/org/kolco24/kolco24/ui/legends/LegendsFragment.java
+++ app/src/main/java/org/kolco24/kolco24/ui/legends/LegendsFragment.java
@@ -1,10 +1,12 @@
 package org.kolco24.kolco24.ui.legends;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,10 +18,22 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import org.kolco24.kolco24.data.Point;
 import org.kolco24.kolco24.databinding.FragmentLegendsBinding;
 
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Call;
+import okhttp3.ResponseBody;
+
 public class LegendsFragment extends Fragment {
 
     private FragmentLegendsBinding binding;
     private PointViewModel mPointViewModel;
+    private final OkHttpClient client = new OkHttpClient();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -44,13 +58,93 @@ public class LegendsFragment extends Fragment {
         //swipe to refresh
         SwipeRefreshLayout swipeRefreshLayout = binding.swipeToRefresh;
         swipeRefreshLayout.setOnRefreshListener(() -> {
-            int random_point = (int) Math.floor(Math.random() * (100));
-            int random_cost = (int) Math.floor(Math.random() * (10));
-            mPointViewModel.insert(new Point(Integer.toString(random_point), "Дерево в лесу", random_cost));
-            swipeRefreshLayout.setRefreshing(false);
+            //http
+            try {
+                run();
+            } catch (IOException e) {
+                // TODO
+                Context context = root.getContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                //
+                e.printStackTrace();
+            }
         });
 
         return root;
+    }
+
+    public void run() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://192.168.88.5:8081")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("Failure");
+                e.printStackTrace();
+
+                SwipeRefreshLayout swipeRefreshLayout = binding.swipeToRefresh;
+                swipeRefreshLayout.setRefreshing(false);
+
+                // toast
+                Context context = binding.getRoot().getContext();
+                Handler handler = new Handler(context.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        CharSequence text = "Ошибка обновления списка, нет связи с сервером";
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    // turn of loader
+                    SwipeRefreshLayout swipeRefreshLayout = binding.swipeToRefresh;
+                    swipeRefreshLayout.setRefreshing(false);
+
+                    //print headers to log
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    // insert in DB
+                    mPointViewModel.deleteAll();
+                    String legend = responseBody.string();
+                    String legend_name = legend.substring(0, 100);
+                    for (int i = 10; i < 50; i++) {
+                        mPointViewModel.insert(new Point(Integer.toString(i), legend_name, 1));
+                    }
+
+                    // toast
+                    Context context = binding.getRoot().getContext();
+                    Handler handler = new Handler(context.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            CharSequence text = "Список обновлен";
+                            int duration = Toast.LENGTH_LONG;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                    });
+                    // logs
+                    System.out.println(legend);
+                }
+            }
+        });
     }
 
     @Override
