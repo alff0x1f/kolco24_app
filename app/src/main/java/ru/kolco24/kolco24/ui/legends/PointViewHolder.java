a60d749c195dbc7d635b1473bd05a00c083e53d8
alff0x1f <alff0x1f@gmail.com>
2022-10-02 18:27:51 +0500

    New legend page, fix colors in night mode

--- app/src/main/java/ru/kolco24/kolco24/ui/legends/PointViewHolder.java
+++ app/src/main/java/ru/kolco24/kolco24/ui/legends/PointViewHolder.java
@@ -1,10 +1,13 @@
 package ru.kolco24.kolco24.ui.legends;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import ru.kolco24.kolco24.R;
@@ -14,6 +17,7 @@ public class PointViewHolder extends RecyclerView.ViewHolder {
     private final TextView textView;
     private final TextView textDescription;
     private final TextView textCost;
+    private final TextView textPointNumber;
 
     /*__init__*/
     private PointViewHolder(View itemView) {
@@ -21,12 +25,41 @@ public class PointViewHolder extends RecyclerView.ViewHolder {
         textView = itemView.findViewById(R.id.textView);
         textDescription = itemView.findViewById(R.id.textDescription);
         textCost = itemView.findViewById(R.id.textCost);
+        textPointNumber = itemView.findViewById(R.id.pointNumber);
     }
 
     public void bind(Point point) {
         textView.setText(String.format("%02d", point.mNumber));
+        textPointNumber.setText(String.format("%02d", point.mNumber));
+
         textDescription.setText(point.mDescription);
-        textCost.setText(String.valueOf(point.mCost));
+        textCost.setText(String.format("+%d", point.mCost));
+        Drawable drawable = ContextCompat.getDrawable(itemView.getContext(), R.drawable.cost_background);
+        // set color based on cost
+        int color = Color.parseColor("#000000");
+        if (point.mCost == 1) {
+            color = itemView.getResources().getColor(R.color.level1);
+        } else if (point.mCost == 2) {
+            color = itemView.getResources().getColor(R.color.level2);
+        } else if (point.mCost == 3) {
+            color = itemView.getResources().getColor(R.color.level3);
+        } else if (point.mCost == 4) {
+            color = itemView.getResources().getColor(R.color.level4);
+        } else if (point.mCost == 5) {
+            color = itemView.getResources().getColor(R.color.level5);
+        } else if (point.mCost == 6) {
+            color = itemView.getResources().getColor(R.color.level6);
+        } else if (point.mCost == 7) {
+            color = itemView.getResources().getColor(R.color.level7);
+        } else if (point.mCost == 8) {
+            color = itemView.getResources().getColor(R.color.level8);
+        } else if (point.mCost == 9) {
+            color = itemView.getResources().getColor(R.color.level9);
+        } else {
+            color = itemView.getResources().getColor(R.color.level10);
+        }
+        drawable.mutate().setColorFilter(color, android.graphics.PorterDuff.Mode.SRC_ATOP);
+        textCost.setBackground(drawable);
     }
 
     /**
