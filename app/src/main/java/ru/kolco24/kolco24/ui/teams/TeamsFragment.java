37e0b7859059268dce65d4a91b0e765fd3083a00
alff0x1f <alff0x1f@gmail.com>
2022-10-13 03:06:23 +0500

    rm unused imports

--- app/src/main/java/ru/kolco24/kolco24/ui/teams/TeamsFragment.java
+++ app/src/main/java/ru/kolco24/kolco24/ui/teams/TeamsFragment.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -26,28 +25,13 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 import ru.kolco24.kolco24.R;
-import ru.kolco24.kolco24.data.Team;
 import ru.kolco24.kolco24.databinding.FragmentTeamsBinding;
 
 public class TeamsFragment extends Fragment {
     private FragmentTeamsBinding binding;
     private TeamViewModel mTeamViewModel;
     private SharedPreferences sharedpreferences;
-
-    private final OkHttpClient client = new OkHttpClient();
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
