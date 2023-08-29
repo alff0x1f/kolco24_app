5639652efc3562ab209fc6895d30c6376541a51e
alff0x1f <alff0x1f@gmail.com>
2023-08-30 01:20:29 +0500

    #1 kotlin app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kolco24"
+include(":app")
+ 
