02dd748e59bbee7614f82dd21ef5cf5b54028338
alff0x1f <alff0x1f@gmail.com>
2023-09-17 14:13:03 +0500

    Add gallery permission

--- app/src/main/java/ru/kolco24/kolco24/NfcPointActivity.kt
+++ app/src/main/java/ru/kolco24/kolco24/NfcPointActivity.kt
@@ -1,13 +1,18 @@
 package ru.kolco24.kolco24
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.Tag
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class NfcPointActivity : AppCompatActivity() {
     private var nfcAdapter: NfcAdapter? = null
@@ -20,6 +25,64 @@ class NfcPointActivity : AppCompatActivity() {
         nfcAdapter = NfcAdapter.getDefaultAdapter(this)
         textView = findViewById(R.id.textView)
         handleIntent(intent)
+        requestPermission()
+        if (lastGalleryImageUri() != null){
+            Toast.makeText(this, lastGalleryImageUri(),  Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Галерея недоступна",  Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES),
+                    1
+                )
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1
+                )
+            }
+        }
+    }
+
+    private fun lastGalleryImageUri(): String? {
+        val projection = arrayOf(MediaStore.Images.Media._ID)
+        val cursor = contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            MediaStore.Images.Media._ID + " DESC"
+        )
+        if (cursor != null && cursor.moveToFirst()) {
+            val index = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            if (index == -1) {
+                cursor.close()
+                return null
+            }
+            val id = cursor.getLong(index)
+            cursor.close()
+            return MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                .buildUpon().appendPath(id.toString()).toString()
+        }
+        return null
     }
 
     private fun handleIntent(intent: Intent) {
@@ -41,13 +104,8 @@ class NfcPointActivity : AppCompatActivity() {
                     for (record in message.records) {
                         val payload = record.payload
                         val text = String(payload)
-                        // Display the NDEF message content
-                        Toast.makeText(this, "NDEF Message: $text, $hexId", Toast.LENGTH_SHORT)
-                            .show()
                         // I want show hexId in TextView, chatGPT do it here
-                        textView.text = "Hex ID: $hexId"
-
-
+                        textView.text = "Hex ID: $hexId, $text"
                     }
                 }
             }
