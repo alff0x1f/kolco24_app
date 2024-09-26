cd1eaeac4c74959956901a58e568a9cb678545d0
alff0x1f <alff0x1f@gmail.com>
2024-09-26 19:02:19 +0500

    add AddTagActivity

---
+++ app/src/main/java/ru/kolco24/kolco24/AddTagActivity.kt
@@ -0,0 +1,18 @@
+package ru.kolco24.kolco24
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddTagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_tag)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
